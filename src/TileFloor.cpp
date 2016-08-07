c3edad0c961206fab82c085dc434cc6e51403040
forgottencreature <patrickwarner@gmail.com>
2016-08-06 22:10:35 -0400

    fixed the camera and some cleanup

--- src/TileFloor.cpp
+++ src/TileFloor.cpp
@@ -19,35 +19,28 @@ void TileFloor::setType(Type t)
         case EMPTY:
             this->color = sf::Color::Transparent;
             this->health = 0;
-            this->identifier = "empty";
             this->passThrough = true;
             break;
         case DIRT:
             this->color = sf::Color(160,82,45);
             this->health = 100;
-            this->identifier = "dirt";
             this->passThrough = true;
             break;
         case SNOW:
             this->color = sf::Color(25,25,112,255);
             this->health = 100;
-            this->identifier = "snow";
             this->passThrough = false;
             break;
         case GRASS:
             this->color = sf::Color::Green;
             this->health = 100;
-            this->identifier = "grass";
             this->passThrough = true;
             break;
         default:
             this->color = sf::Color::Transparent;
             this->health = 0;
-            this->identifier = "default";
             this->passThrough = true;
     }
-
-    std::cout << "UPDATING TILE FLOOR TYPE TO " << this->identifier << "\n";
 
 }
 
@@ -69,11 +62,6 @@ void TileFloor::setHealth(int h)
 int TileFloor::getHealth()
 {
     return this->health;
-}
-
-std::string TileFloor::getIdentity()
-{
-    return this->identifier;
 }
 
 bool TileFloor::canPassThrough()
